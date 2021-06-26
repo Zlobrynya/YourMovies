9fa7660bb77cd7105583f87b14de8b675f0a114f
Zlobrynya <zlobrynya@gmail.com>
2021-06-26 17:24:47 +0300

    Initial Commit

---
+++ YourMovies/YourMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourMoviesApp.swift
+//  YourMovies
+//
+//  Created by Nikita Nikitin on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct YourMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
