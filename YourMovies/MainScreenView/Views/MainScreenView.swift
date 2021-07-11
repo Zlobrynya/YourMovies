221e7db318d75fb1ab67626e30fe06be68ba405e
Zlobrynya <zlobrynya@gmail.com>
2021-07-11 18:25:57 +0300

    Add MainScreenView

---
+++ YourMovies/MainScreenView/Views/MainScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenView.swift
+//  YourMovies
+//
+//  Created by Nikita Nikitin on 11.07.2021.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenView()
+    }
+}
